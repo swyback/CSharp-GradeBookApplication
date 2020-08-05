8dae7b8229f648273a6dfa931a52d1adc33e08fa
Richie Birkett <Richie.Birkett@airsweb.com>
2020-08-05 08:46:54 +0100

    Module 3 Override RankedGradeBooks GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,26 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("cannot be less than 5 students");
+            }
+
+
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60 && averageGrade <=80)
+                return 'B';
+            else if (averageGrade >= 40 && averageGrade <=60)
+                return 'C';
+            else if (averageGrade >= 20 && averageGrade <=40)
+                return 'D';
+            else
+                return 'F';
+          
+        }
     }
 }
